70993976eb7bb50f9762291ab88fbf3d57187c08
sayen akira <amardjebabla10@gmail.com>
2020-11-29 16:56:39 +0100

    current work

--- wefund/frontend/src/components/Nav.js
+++ wefund/frontend/src/components/Nav.js
@@ -1,39 +1,38 @@
 import React, {Component, component} from 'react';
-
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import login    from './content/signUp/Part1';
 const Nav = () => {
   // const users=this.state.users
   return (
-    <div>
-      <nav className="navbar navbar-center-lg navbar-light bg-white">
-        <img src="../static/img/Asset 2@4x@2x.png" height={36} width={45} alt="we fund"/>
-        <input type="text" name="search" placeholder="Search"/>
-        <input type="submit" defaultValue="Search !"/>
-        <div className="lang">
-          <button name="en" id="en">
-            En |
-          </button>
-          <button name="en" id="ar">Ar
-          </button>
-        </div>
-        <a href="#" id="home">
-          Home
-        </a>
-        <a href="#dowbottomn" id="about">About us
-        </a>
-        <a href="#mihrajan" id="event">
-          Events
-        </a>
-        <a href="#donate" id="supporting">
-          Supporting us
-        </a>
-        <a href="#dowbottomn" id="contact">
-          Contact us
-        </a>
-        <img src="../static/img/Group 729@2x.png" height={21} width={25}/>
-      </nav>
-    </div>
+    <Router>
+      <div>
+        <nav className="navbar navbar-center-lg navbar-light bg-white">
+          <img src="../static/img/Asset 2@4x@2x.png" height={36} width={45} alt="we fund"/>
+          <input type="text" name="search" placeholder="Search"/>
+          <input type="submit" defaultValue="Search !"/>
+          <div className="lang">
+            <button name="en" id="en">
+              En |
+            </button>
+            <button name="en" id="ar">Ar
+            </button>
+          </div>
+          <Link to={'/'} className="nav-link"> Home </Link>
+          <Link to={'/login'} className="nav-link"> Login/Register </Link>
+          <Link to={'/about'} className="nav-link"> About us </Link>
+          <Link to={'/event'} className="nav-link"> Events </Link>
+          <Link to={'/support'} className="nav-link"> Supporting us </Link>
+          <Link to={'/contact'} className="nav-link"> Contact us </Link>
 
+
+          <img src="../static/img/Group 729@2x.png" height={21} width={25}/>
+        </nav>
+      </div>
+          <Switch>
+              <Route exact path="/login" component={login}/>
+          </Switch>
+    </Router>
+    
   );
 }
 
