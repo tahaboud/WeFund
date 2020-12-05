a2cd58b1af9d04de551eb94a56c06333f5e84ab6
sayen akira <amardjebabla10@gmail.com>
2020-12-05 10:37:08 +0100

    step10 :full website

--- wefund/wefund/urls.py
+++ wefund/wefund/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('frontend.urls')),
     path('api/admin/', admin.site.urls),
     path('api/account/', include("accounts.urls")),
     path('api/research/', include("researches.urls")),
