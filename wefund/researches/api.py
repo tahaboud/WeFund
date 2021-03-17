cf247db0b66032ba5a8db9ea96743a254219b2ae
tahaboud <tahaboudouaoui@gmail.com>
2021-03-17 12:35:17 +0100

    fixing gitignore and research pk retrieve

--- wefund/researches/api.py
+++ wefund/researches/api.py
@@ -60,7 +60,7 @@ class AdminResearchAPI(viewsets.ModelViewSet):
     def retrieve(self, request, pk):
         user = Account.objects.get(pk=pk)
         if hasattr(user, "researcher"):
-            if hasattr(user, "research"):
+            if hasattr(user.researcher, "research"):
                 research = user.researcher.research
                 serializer = AdminResearchSerializer(research)
                 return Response(serializer.data)
